d7c7440cf026e3b4089c47ff97a3f8caa31d121f
dimayermak <dimayermak@example.com>
2026-01-15 17:49:06 +0200

    chore: add google site verification metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,9 @@ export const metadata: Metadata = {
             'max-snippet': -1,
         },
     },
+    verification: {
+        google: "yreuEw4yGSs4xoZ2uk6wKRoQn8bErQreK_Xhxw_y4G0",
+    },
 };
 
 export default function RootLayout({
